dbb605568e1bf730fb649abf85c686c3d4ff7980
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-16 16:04:24 -0500

    Create gregorian_util.hpp
    Incomplete

---
+++ include/simplydt/gregorian_calendar/gregorian_util.hpp
@@ -0,0 +1,41 @@
+
+/*!
+* @file gregorian_util.hpp
+*
+* @brief
+* Gregorian calendar utilities document.
+*
+* @details
+* Utilities for Gregorian calendar related functionality.
+*/
+
+
+
+#ifndef SIMPLYDT_L_SIMPLYDT_GREGORIAN_DEFS_H_
+#define SIMPLYDT_L_SIMPLYDT_GREGORIAN_DEFS_H_
+
+
+
+namespace SimplyDt {
+
+    namespace GregorianCalendar {
+
+        /*!
+        * @namespace SimplyDt::GregorianCalendar::Util
+        *
+        * @brief
+        * Standard civil calendar utilities namespace.
+        */
+        namespace Util {
+
+            //
+
+        }
+
+    }
+
+}
+
+
+
+#endif // SIMPLYDT_L_SIMPLYDT_GREGORIAN_DEFS_H_
