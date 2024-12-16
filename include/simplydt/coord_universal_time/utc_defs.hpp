af3fb3318124a424b8e9dd41f745ab7e3fdb144b
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-16 15:40:13 -0500

    Create utc_defs.hpp
    Incomplete

---
+++ include/simplydt/coord_universal_time/utc_defs.hpp
@@ -0,0 +1,38 @@
+
+/*!
+* @file utc_defs.hpp
+*
+* @brief
+* Coordinated Universal Time definitions document.
+*
+* @details
+* Constant definitions for Coordinated Universal Time
+* related entities.
+*/
+
+
+
+#ifndef SIMPLYDT_L_SIMPLYDT_UTC_DEFS_H_
+#define SIMPLYDT_L_SIMPLYDT_UTC_DEFS_H_
+
+
+
+namespace SimplyDt {
+
+    /*!
+    * @namespace SimplyDt::CoordUniversalTime
+    *
+    * @brief
+    * Coordinated Universal Time system namespace.
+    */
+    namespace CoordUniversalTime {
+
+        //
+
+    }
+
+}
+
+
+
+#endif // SIMPLYDT_L_SIMPLYDT_UTC_DEFS_H_
