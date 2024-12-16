21f7ba9bd0e069a9fc7b40b5feaae9f4d6a97603
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-16 15:35:17 -0500

    Create julian_defs.hpp
    Incomplete

---
+++ include/simplydt/julian_calendar/julian_defs.hpp
@@ -0,0 +1,38 @@
+
+/*!
+* @file julian_defs.hpp
+*
+* @brief
+* Julian calendar definitions document.
+*
+* @details
+* Constant definitions for Julian calendar related
+* entities.
+*/
+
+
+
+#ifndef SIMPLYDT_L_SIMPLYDT_JULIAN_DEFS_H_
+#define SIMPLYDT_L_SIMPLYDT_JULIAN_DEFS_H_
+
+
+
+namespace SimplyDt {
+
+    /*!
+    * @namespace SimplyDt::JulianCalendar
+    *
+    * @brief
+    * Roman solar calendar system namespace.
+    */
+    namespace JulianCalendar {
+
+        //
+
+    }
+
+}
+
+
+
+#endif // SIMPLYDT_L_SIMPLYDT_JULIAN_DEFS_H_
