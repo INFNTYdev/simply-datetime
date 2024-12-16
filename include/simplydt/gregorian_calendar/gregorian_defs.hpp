0d675ff99f62f31562857878c0dcb507823bb319
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-16 17:58:32 -0500

    Update gregorian_defs.hpp

--- include/simplydt/gregorian_calendar/gregorian_defs.hpp
+++ include/simplydt/gregorian_calendar/gregorian_defs.hpp
@@ -17,7 +17,12 @@
 
 
 
-namespace SimplyDt {
+#include<cstdint>
+#include<array>
+
+
+namespace SimplyDt
+{
 
     /*!
     * @namespace SimplyDt::GregorianCalendar
@@ -25,9 +30,76 @@ namespace SimplyDt {
     * @brief
     * Standard civil calendar system namespace.
     */
-    namespace GregorianCalendar {
+    namespace GregorianCalendar
+    {
 
-        //
+        /*!
+        * @brief
+        * Gregorian date presentation formats.
+        */
+        enum DateFormat {
+            RECORD,     ///< Record format (01-01-1970)
+            STANDARD    ///< Standard format (01/01/1970)
+        };
+
+        enum DateLayout {
+            M_D_YYYY,    ///< mm/dd/YYYY [ Example: 01-29-1970 ]
+		    M_D_YY,      ///< mm/dd/YY [ Example: 01-29-70 ]
+		    M_YYYY,      ///< mm/YYYY [ Example: 01-1970 ]
+		    M_YY,        ///< mm/YY [ Example: 01-70 ]
+		    M_D,         ///< mm/dd [ Example: 01-29 ]
+		    YYYY_M_D,    ///< YYYY/mm/dd [ Example: 1970-01-29 ]
+		    YY_M_D,      ///< YY/mm/dd [ Example: 70-01-29 ]
+		    YYYY_D_M,    ///< YYYY/dd/mm [ Example: 1970-29-01 ]
+		    YY_D_M,      ///< YY/dd/mm [ Example: 70-29-01 ]
+		    YYYY_M,      ///< YYYY/mm [ Example: 1970-01 ]
+		    YY_M,        ///< YY/mm [ Example: 70-01 ]
+		    D_M,         ///< dd/mm [ Example: 29-01 ]
+        };
+
+        /*!
+        * @brief
+        * Gregorian calendar date.
+        */
+        struct Date
+        {
+            const uint16_t year;    ///< Date year
+            const uint8_t month;    ///< Date month
+            const uint8_t day;      ///< Date day
+        };
+
+        /*!
+        * @brief
+        * Gregorian calendar days of week.
+        */
+        const std::array<const char*, 7> DaysOfWeek{
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        };
+
+        /*!
+        * @brief
+        * Gregorian calendar months.
+        */
+        const std::array<const char*, 12> Months{
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        };
 
     }
 
