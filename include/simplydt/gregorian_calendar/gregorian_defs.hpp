dd1e357d23a8871ae061eb8f18a20b375dbc7613
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-16 15:35:10 -0500

    Create gregorian_defs.hpp
    Incomplete

---
+++ include/simplydt/gregorian_calendar/gregorian_defs.hpp
@@ -0,0 +1,38 @@
+
+/*!
+* @file gregorian_defs.hpp
+*
+* @brief
+* Gregorian calendar definitions document.
+*
+* @details
+* Constant definitions for Gregorian calendar related
+* entities.
+*/
+
+
+
+#ifndef SIMPLYDT_L_SIMPLYDT_GREGORIAN_DEFS_H_
+#define SIMPLYDT_L_SIMPLYDT_GREGORIAN_DEFS_H_
+
+
+
+namespace SimplyDt {
+
+    /*!
+    * @namespace SimplyDt::GregorianCalendar
+    *
+    * @brief
+    * Standard civil calendar system namespace.
+    */
+    namespace GregorianCalendar {
+
+        //
+
+    }
+
+}
+
+
+
+#endif // SIMPLYDT_L_SIMPLYDT_GREGORIAN_DEFS_H_
