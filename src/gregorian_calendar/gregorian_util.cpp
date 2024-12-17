906d7e5e4c842fc6b528c6cea6d5ba767ac2502b
Jamon Bailey <infinity.dev@outlook.com>
2024-12-17 10:50:46 -0500

    Update gregorian_util.cpp

--- src/gregorian_calendar/gregorian_util.cpp
+++ src/gregorian_calendar/gregorian_util.cpp
@@ -3,3 +3,8 @@
 
 
 //  SimplyDt::GregorianCalendar::Util : FREE
+
+bool SimplyDt::GregorianCalendar::Util::isLeapYear(const uint16_t& year) noexcept
+{
+	return ((year % (uint16_t)4) == 0 && (year % (uint16_t)100 != 0 || year % (uint16_t)400 == 0));
+}
