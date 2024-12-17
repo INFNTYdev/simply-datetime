298bfddda171ee927657cea64c3bd8e7702700cd
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-17 06:50:53 -0500

    Create string_util.hpp
    Incomplete

---
+++ include/simplydt/common/string/string_util.hpp
@@ -0,0 +1,40 @@
+
+/*!
+* @file string_util.hpp
+*
+* @brief
+* String utilities document.
+*
+* @details
+* Utilities for datetime related strings.
+*/
+
+
+
+#ifndef SIMPLYDT_L_SIMPLYDT_DATETIME_STRING_UTIL_H_
+#define SIMPLYDT_L_SIMPLYDT_DATETIME_STRING_UTIL_H_
+
+
+
+#include<string>
+
+
+namespace SimplyDt {
+
+    /*!
+    * @namespace SimplyDt::String
+    *
+    * @brief
+    * Datetime string utilities namespace.
+    */
+    namespace String {
+
+        //
+
+    }
+
+}
+
+
+
+#endif // SIMPLYDT_L_SIMPLYDT_DATETIME_STRING_UTIL_H_
