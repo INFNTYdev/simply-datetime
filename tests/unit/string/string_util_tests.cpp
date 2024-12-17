ddc5175b90748aec30a93d559099bba9ab12980d
Jamon Bailey <infinity.dev@outlook.com>
2024-12-17 09:01:24 -0500

    Create string_util_tests.cpp

---
+++ tests/unit/string/string_util_tests.cpp
@@ -0,0 +1,36 @@
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+//    DATETIME STRING UTILITY TESTS     //
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+
+
+
+#include<gtest/gtest.h>
+#include<cstdint>
+#include<vector>
+#include"simplydt/common/string/string_util.hpp"
+
+
+namespace DTStringUtilityTests {
+
+	TEST(StringUtilityTestSuite, IntegerToDoubleDigitString)
+	{
+		std::vector<std::pair<uint16_t, std::string>> testSet{
+			{ 2024, "24" },
+			{ 2000, "00" },
+			{ 1970, "70" },
+			{ 4444, "44" },
+			{ 7777, "77" },
+		};
+
+		for (std::pair<uint16_t, std::string>& set : testSet) {
+
+			const uint16_t* test = &set.first;
+			const char* expect = set.second.data();
+
+			ASSERT_EQ(SimplyDt::String::toDoubleDigitStr(*test), expect);
+
+		}
+	}
+
+}
