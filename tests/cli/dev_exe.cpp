f3dc8e0bfeb2ff1a72efe584689139ae65d81727
Jamon Bailey <83630529+ViRtigo23@users.noreply.github.com>
2024-12-17 07:40:18 -0500

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -1,11 +1,16 @@
 
 #include<iostream>
+#include"simplydt/common/string/string_util.hpp"
 
 
 int main(int argc, char* argv[])
 {
     //
     std::cout << "\nWorking properly." << std::endl;
+
+    unsigned long int testYear{ 2024 };
+
+    std::string demo{ SimplyDt::String::toDoubleDigitStr(testYear) };
     
     return 0;
 }
