db00339ceb59a6a4533d1f67e77c67c7f390a492
Jamon Bailey <infinity.dev@outlook.com>
2024-12-18 18:04:08 -0500

    Update greg_calen_tests.cpp

--- tests/unit/gregorian_calendar/greg_calen_tests.cpp
+++ tests/unit/gregorian_calendar/greg_calen_tests.cpp
@@ -91,4 +91,69 @@ namespace DTStringUtilityTests {
 		}
 	}
 
+	TEST(GregorianCalendarUtilityTestSuite, DetermineMonthLiteralFromInteger)
+	{
+		std::vector<std::pair<uint8_t, std::string>> testSet{
+			{1, "January"},
+			{2, "February"},
+			{3, "March"},
+			{4, "April"},
+			{5, "May"},
+			{6, "June"},
+			{7, "July"},
+			{8, "August"},
+			{9, "September"},
+			{10, "October"},
+			{11, "November"},
+			{12, "December"}
+		};
+
+		uint8_t sample{ 255 };
+
+		while (sample >= (uint8_t)0) {
+
+			const bool isValidIndex = (sample >= 1 && sample <= 12);
+
+			if (isValidIndex) {
+				const char* expect = testSet[sample - 1].second.data();
+				ASSERT_EQ(SimplyDt::GregorianCalendar::Util::getMonth(sample), expect);
+			}
+			else {
+				const std::string expect{ "January" };
+				ASSERT_EQ(SimplyDt::GregorianCalendar::Util::getMonth(sample), expect);
+			}
+
+			if (sample == 0)
+				break;
+
+			--sample;
+
+		}
+	}
+
+	TEST(GregorianCalendarUtilityTestSuite, DetermineDateDayOfWeekIndex)
+	{
+		std::vector<std::pair<SimplyDt::GregorianCalendar::Date, uint8_t>> testSet{
+			{{.year=2024, .month=12, .day=18}, 3},
+		};
+
+		for (std::pair<SimplyDt::GregorianCalendar::Date, uint8_t>& set : testSet) {
+
+			const uint16_t* sampleYear = &set.first.year;
+			const uint8_t* sampleMonth = &set.first.month;
+			const uint8_t* sampleDay = &set.first.day;
+			const uint8_t* expect = &set.second;
+
+			ASSERT_EQ(
+				SimplyDt::GregorianCalendar::Util::getDayOfWeekIndex(
+					*sampleYear,
+					*sampleMonth,
+					*sampleDay
+				),
+				*expect
+			);
+
+		}
+	}
+
 }
