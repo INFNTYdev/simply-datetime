243ea6ce280131f8d326ee5636d3c719a51dcd61
Jamon Bailey <infinity.dev@outlook.com>
2024-12-19 15:48:20 -0500

    Create utc_util.hpp
    Incomplete

---
+++ include/simplydt/coord_universal_time/utc_util.hpp
@@ -0,0 +1,48 @@
+
+/*!
+* @file utc_util.hpp
+*
+* @brief
+* Coordinated Universal Time utilities document.
+*
+* @details
+* Utilities for Coordinated Universal Time related
+* functionality.
+*/
+
+
+
+#ifndef SIMPLYDT_L_SIMPLYDT_COORD_UNIVERSAL_TIME_UTIL_H_
+#define SIMPLYDT_L_SIMPLYDT_COORD_UNIVERSAL_TIME_UTIL_H_
+
+
+
+#include<cstdint>
+#include"simplydt/coord_universal_time/utc_defs.hpp"
+#include"simplydt/gregorian_calendar/gregorian_defs.hpp"
+#include"simplydt/julian_calendar/julian_defs.hpp"
+
+
+namespace SimplyDt {
+
+    namespace CoordUniversalTime {
+
+        /*!
+        * @namespace SimplyDt::CoordUniversalTime::Util
+        *
+        * @brief
+        * Standard time system utilities namespace.
+        */
+        namespace Util {
+
+            //
+
+        }
+
+    }
+
+}
+
+
+
+#endif // SIMPLYDT_L_SIMPLYDT_COORD_UNIVERSAL_TIME_UTIL_H_
