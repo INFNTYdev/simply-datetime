f121fa9d71157f8e855eb2f1f1a1dce0249d3109
Jamon Bailey <infinity.dev@outlook.com>
2024-12-21 20:25:55 -0500

    Update greg_util_bm.cpp

--- tests/benchmark/gregorian_calendar/greg_util_bm.cpp
+++ tests/benchmark/gregorian_calendar/greg_util_bm.cpp
@@ -1,2 +1,24 @@
 
-//
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+//    GREGORIAN CALENDAR UTILITY BENCHMARK TESTS    //
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
+
+
+
+#include<random>
+#include<benchmark/benchmark.h>
+#include"simplydt/gregorian_calendar/gregorian_util.hpp"
+
+
+static void validateYear_bm(benchmark::State& state)
+{
+	const uint16_t testYear{ 2024 };
+
+	for (auto _ : state) {
+
+		benchmark::DoNotOptimize(
+			SimplyDt::GregorianCalendar::Util::isValidYear(testYear)
+		);
+
+	}
+}
