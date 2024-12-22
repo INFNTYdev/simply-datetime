17beceadeb5561738fcf6d778057a7add3063982
Jamon Bailey <infinity.dev@outlook.com>
2024-12-21 22:22:11 -0500

    Update greg_util_bm.cpp

--- tests/benchmark/gregorian_calendar/greg_util_bm.cpp
+++ tests/benchmark/gregorian_calendar/greg_util_bm.cpp
@@ -118,6 +118,121 @@ namespace GregorianCalendarBenchmark {
         }
     }
     BENCHMARK(BM_calculateYearTotalDays);
+
+    static void BM_calculateMonthIndex(benchmark::State& state)
+    {
+        const uint8_t testMonth{ 5 };
+
+        for (auto _ : state) {
+
+            benchmark::DoNotOptimize(
+                SimplyDt::GregorianCalendar::Util::getMonthIndex(testMonth)
+            );
+
+        }
+    }
+    BENCHMARK(BM_calculateMonthIndex);
+
+    static void BM_getMonthName(benchmark::State& state)
+    {
+        const uint8_t testMonth{ 7 };
+
+        for (auto _ : state) {
+
+            benchmark::DoNotOptimize(
+                SimplyDt::GregorianCalendar::Util::getMonth(testMonth)
+            );
+
+        }
+    }
+    BENCHMARK(BM_getMonthName);
+
+    static void BM_getMonthAbbreviation(benchmark::State& state)
+    {
+        const uint8_t testMonth{ 1 };
+
+        for (auto _ : state) {
+
+            benchmark::DoNotOptimize(
+                SimplyDt::GregorianCalendar::Util::getMonthAbbrev(testMonth)
+            );
+
+        }
+    }
+    BENCHMARK(BM_getMonthAbbreviation);
+
+    static void BM_calculateDayOfWeekIndex(benchmark::State& state)
+    {
+        const uint16_t testYear{ 2016 };
+        const uint8_t testMonth{ 6 };
+        const uint8_t testDay{ 19 };
+
+        for (auto _ : state) {
+
+            benchmark::DoNotOptimize(
+                SimplyDt::GregorianCalendar::Util::getDayOfWeekIndex(
+                    testYear,
+                    testMonth,
+                    testDay
+                )
+            );
+
+        }
+    }
+    BENCHMARK(BM_calculateDayOfWeekIndex);
+
+    static void BM_getDayOfWeekName(benchmark::State& state)
+    {
+        const uint16_t testYear{ 2014 };
+        const uint8_t testMonth{ 9 };
+        const uint8_t testDay{ 21 };
+
+        for (auto _ : state) {
+
+            benchmark::DoNotOptimize(
+                SimplyDt::GregorianCalendar::Util::getDayOfWeek(
+                    testYear,
+                    testMonth,
+                    testDay
+                )
+            );
+
+        }
+    }
+    BENCHMARK(BM_getDayOfWeekName);
+
+    static void BM_getDayOfWeekAbbreviation(benchmark::State& state)
+    {
+        const uint16_t testYear{ 1972 };
+        const uint8_t testMonth{ 12 };
+        const uint8_t testDay{ 21 };
+
+        for (auto _ : state) {
+
+            benchmark::DoNotOptimize(
+                SimplyDt::GregorianCalendar::Util::getDayOfWeekAbbrev(
+                    testYear,
+                    testMonth,
+                    testDay
+                )
+            );
+
+        }
+    }
+    BENCHMARK(BM_getDayOfWeekAbbreviation);
+
+    static void BM_calculateDateFromJdn(benchmark::State& state)
+    {
+        const SimplyDt::JulianCalendar::JDN testJdn{ 2'440'587.5 };
+        SimplyDt::GregorianCalendar::Date testDate{};
+
+        for (auto _ : state) {
+
+            SimplyDt::GregorianCalendar::Util::jdnToDate(testJdn, testDate);
+
+        }
+    }
+    BENCHMARK(BM_calculateDateFromJdn);
     
 }
 
