d8ec0ce6374c1cb8de23a9603960dcafc8e35095
Jamon Bailey <infinity.dev@outlook.com>
2024-12-24 23:34:33 -0500

    Create Simply Datetime Python Package

---
+++ python/simplydt/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+     Simply Datetime Library
+     (Python Implementation)
+
+     Infinity Systems, LLC. 2025
+
+     Author(s): Jamon Bailey
+     Written in: CXX, Cython, Python
+"""
+
+
+pass
